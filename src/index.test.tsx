dc0fb90a8d3e7e6c6b0ec4aac728d63470761ca9
Ben Hur Martins Carvalho <benhur.martins@hotmail.com>
2022-04-14 13:49:12 -0300

    feat: refactor and ci/cd

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,7 +1,38 @@
-import { ExampleComponent } from '.'
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { PropsManagerProvider, usePropsContext } from '.'
 
-describe('ExampleComponent', () => {
-  it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy()
+type TestPropsType = {
+  testProps: string
+}
+
+const TestComponent = () => {
+  const { getProps } = usePropsContext()
+  const { testProps } = getProps('testId') as TestPropsType
+
+  return (
+    <div>
+      <p>{testProps}</p>
+    </div>
+  )
+}
+
+const propsValue = {
+  testId: {
+    testProps: 'some value'
+  }
+}
+
+const component = (
+  <PropsManagerProvider propsValue={propsValue}>
+    <TestComponent />
+  </PropsManagerProvider>
+)
+
+describe('PropsManagerProvider', () => {
+  it('should show test value inside the component', () => {
+    render(component)
+    expect(screen.getByText(/^some value/)).toBeTruthy()
   })
 })
